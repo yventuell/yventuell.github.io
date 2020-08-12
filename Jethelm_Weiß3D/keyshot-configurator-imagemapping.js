5090ab90513e6da682d5d89d4b4b323dcfe776fc
yventuell <69502724+yventuell@users.noreply.github.com>
2020-08-12 16:09:54 +0200

    a

---
+++ Jethelm_Weiß3D/keyshot-configurator-imagemapping.js
@@ -0,0 +1,107 @@
+imagemappingdata =
+[
+    {
+        "image": "Jethelm_Weiß.3.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "35+212",
+            "49+192",
+            "51+207",
+            "52+198"
+        ],
+        "studio": 1331043206
+    },
+    {
+        "image": "Jethelm_Weiß.3.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "35+219",
+            "49+192",
+            "51+207",
+            "52+198"
+        ],
+        "studio": 1331043206
+    },
+    {
+        "image": "Jethelm_Weiß.3.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "35+212",
+            "49+191",
+            "51+206",
+            "52+197"
+        ],
+        "studio": 1331043206
+    },
+    {
+        "image": "Jethelm_Weiß.3.4.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "35+219",
+            "49+191",
+            "51+206",
+            "52+197"
+        ],
+        "studio": 1331043206
+    },
+    {
+        "image": "Jethelm_Weiß.3.5.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "35+212",
+            "49+192",
+            "51+207",
+            "52+198"
+        ],
+        "studio": 1331043208
+    },
+    {
+        "image": "Jethelm_Weiß.3.6.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "35+219",
+            "49+192",
+            "51+207",
+            "52+198"
+        ],
+        "studio": 1331043208
+    },
+    {
+        "image": "Jethelm_Weiß.3.7.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "35+212",
+            "49+191",
+            "51+206",
+            "52+197"
+        ],
+        "studio": 1331043208
+    },
+    {
+        "image": "Jethelm_Weiß.3.8.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "35+219",
+            "49+191",
+            "51+206",
+            "52+197"
+        ],
+        "studio": 1331043208
+    }
+];
---
+++ Jethelm_Weiß3D/keyshot-configurator-modeldata.js
@@ -0,0 +1,74 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "wei%c3%9f%20%2f%20transparent.png",
+                    "multimaterials": [
+                        "52+198",
+                        "49+192",
+                        "51+207",
+                        "35+212"
+                    ],
+                    "name": "Weiß / Transparent"
+                },
+                {
+                    "icon": "wei%c3%9f%20%2f%20get%c3%b6nt.png",
+                    "multimaterials": [
+                        "52+198",
+                        "49+192",
+                        "51+207",
+                        "35+219"
+                    ],
+                    "name": "Weiß / Getönt"
+                },
+                {
+                    "icon": "grau%20%2f%20transparent.png",
+                    "multimaterials": [
+                        "52+197",
+                        "49+191",
+                        "51+206",
+                        "35+212"
+                    ],
+                    "name": "Grau / Transparent"
+                },
+                {
+                    "icon": "grau%20%2f%20get%c3%b6nt.png",
+                    "multimaterials": [
+                        "52+197",
+                        "49+191",
+                        "51+206",
+                        "35+219"
+                    ],
+                    "name": "Grau / Getönt"
+                }
+            ],
+            "name": "Crivit - Jethelm"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "materialgroups": [
+                "Crivit - Jethelm"
+            ],
+            "modelset": 1005,
+            "name": "Default"
+        }
+    ],
+    "studios": [
+        {
+            "icon": "perspektive.png",
+            "id": 1331043206,
+            "name": "Perspektive"
+        },
+        {
+            "checked": true,
+            "icon": "front.png",
+            "id": 1331043208,
+            "name": "Front"
+        }
+    ]
+};
